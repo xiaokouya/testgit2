15eed173dc8a42b13e157e2b3b3a47e1442a5264
zhao <2971799374@qq.com>
2020-04-30 12:26:32 +0800

    v1.0 init

---
+++ src/test/java/com/example/testgit2/Testgit2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testgit2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Testgit2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
