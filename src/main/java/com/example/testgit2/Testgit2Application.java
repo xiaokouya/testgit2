3d7e12994f9c0a501a7ab98fe223d7a2190935ee
xiaokouya <36222738+xiaokouya@users.noreply.github.com>
2020-04-30 12:36:53 +0800

    Update Testgit2Application.java

--- src/main/java/com/example/testgit2/Testgit2Application.java
+++ src/main/java/com/example/testgit2/Testgit2Application.java
@@ -9,6 +9,7 @@ public class Testgit2Application {
     public static void main(String[] args) {
         SpringApplication.run(Testgit2Application.class, args);
         System.out.println();
+        System.out.println();
     }
 
 }
